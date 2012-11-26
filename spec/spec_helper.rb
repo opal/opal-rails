bb7532425912df19f9f16798b027f01ab63dc792
Steven R. Farley <srfarley@gmail.com>
2012-11-26 06:23:54 -0500

    Resolved Capybara deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,5 +19,5 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.mock_with :rspec
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.include Capybara
+  config.include Capybara::DSL
 end
