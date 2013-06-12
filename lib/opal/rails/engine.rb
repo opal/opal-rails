875feb77dc0c1d0069ecb30239e4e03029761436
Elia Schito <elia@schito.me>
2013-06-12 11:56:33 +0200

    Use a single after_initialize

--- lib/opal/rails/engine.rb
+++ lib/opal/rails/engine.rb
@@ -14,27 +14,23 @@ module Opal
       # and its .rb files are eagerly loaded.
       config.eager_load_paths
 
-      initializer 'opal.asset_paths', :after => 'sprockets.environment', :group => :all do |app|
-        app.config.before_initialize do
-          app.config.eager_load_paths = app.config.eager_load_paths.dup - Dir["#{app.root}/app/{assets,views}"]
-        end
+      config.before_initialize do |app|
+        app.config.eager_load_paths = app.config.eager_load_paths.dup - Dir["#{app.root}/app/{assets,views}"]
+      end
 
+      config.after_initialize do |app|
         Opal.paths.each do |path|
           app.assets.append_path path
         end
 
         Opal.default_options = config.opal
-      end
 
-      config.after_initialize do |app|
         config = app.config
         maps_app = Opal::SourceMapServer.new(app.assets)
 
-        if config.opal.source_map_enabled
-          app.routes.prepend do
-            mount maps_app => maps_app.prefix
-            get '/opal_spec' => 'opal_spec#run'
-          end
+        app.routes.prepend do
+          mount maps_app => maps_app.prefix
+          get '/opal_spec' => 'opal_spec#run'
         end
       end
 
