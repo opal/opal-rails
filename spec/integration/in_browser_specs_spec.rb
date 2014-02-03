f89283cd1d89fa52b92782da318b847cf41568ed
Andrius Chamentauskas <andrius.chamentauskas@gmail.com>
2014-02-03 18:40:58 +0200

    Fixed specs failing because of wrong file order

--- spec/integration/in_browser_specs_spec.rb
+++ spec/integration/in_browser_specs_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 describe 'In-browser specs runner' do
   it 'runs all specs', :js do
     visit '/opal_spec'
-    page.should have_content('Running: subdirectory/example_spec.js example_spec')
+    page.should have_content(' subdirectory/other_spec.js ')
+    page.should have_content(' example_spec.js ')
     page.should have_content('2 examples, 0 failures')
   end
 
   it "runs single spec file", :js do
     visit '/opal_spec'
-    click_link 'subdirectory/example_spec.js'
-    page.should have_content('Running: subdirectory/example_spec.js')
+    click_link 'subdirectory/other_spec.js'
+    page.should have_content('Running: subdirectory/other_spec.js')
     page.should have_content('1 examples, 0 failures')
   end
 end
