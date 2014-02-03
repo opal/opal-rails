3dc4ff6ac13739c8f732ffdaeeac9cf3e158fda4
Andrius Chamentauskas <andrius.chamentauskas@gmail.com>
2014-02-03 18:14:25 +0200

    Fixed specs checking all opal specs run successfully, added spec to check running single spec file

--- spec/integration/in_browser_specs_spec.rb
+++ spec/integration/in_browser_specs_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe 'In-browser specs runner' do
   it 'runs all specs', :js do
     visit '/opal_spec'
-    page.should have_content('Running: example_spec')
+    page.should have_content('Running: subdirectory/example_spec.js example_spec')
+    page.should have_content('2 examples, 0 failures')
+  end
+
+  it "runs single spec file", :js do
+    visit '/opal_spec'
+    click_link 'subdirectory/example_spec.js'
+    page.should have_content('Running: subdirectory/example_spec.js')
     page.should have_content('1 examples, 0 failures')
   end
 end
