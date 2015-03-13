cf5f0bf0a4a37bb376c4fb2732ab017ba61cdbf7
Elia Schito <elia@schito.me>
2015-03-13 01:08:10 +0100

    Use the new source map server
    That is supposed to be mounted on a different location than sprockets.

--- lib/opal/rails/engine.rb
+++ lib/opal/rails/engine.rb
@@ -37,9 +37,12 @@ module Opal
 
         app.routes.prepend do
           if Opal::Processor.source_map_enabled && config.assets.compile
-            prefix = app.config.assets.prefix
-            maps_app = Opal::SourceMapServer.new(app.assets, prefix)
-            mount Rack::Cascade.new([maps_app, app.assets]) => prefix
+            maps_prefix = '/__OPAL_SOURCE_MAPS__'
+            maps_app    = Opal::SourceMapServer.new(app.assets, maps_prefix)
+
+            ::Opal::Sprockets::SourceMapHeaderPatch.inject!(maps_prefix)
+
+            mount maps_app => maps_prefix
           end
 
           get '/opal_spec' => 'opal_spec#run'
