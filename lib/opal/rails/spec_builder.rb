7a10e17951ce3ed8970fb306c60f9041edb64a08
Elia Schito <elia@schito.me>
2015-03-20 00:57:56 +0100

    Cleanup the spec_builder and split in smaller meth

--- lib/opal/rails/spec_builder.rb
+++ lib/opal/rails/spec_builder.rb
@@ -11,26 +11,36 @@ module Opal
       attr_reader :sprockets, :pattern, :spec_location, :root
 
       def to_s
-        path_finder = Opal::HikePathFinder.new(Opal.paths + sprockets.paths)
-
-        builder = Opal::Builder.new(
-          compiler_options: Opal::Processor.compiler_options,
-          stubs:            Opal::Processor.stubbed_files,
-          path_reader:      Opal::PathReader.new(path_finder),
-        )
-
-        builder.build 'opal'
-        builder.build 'opal-rspec'
-
-        spec_files.each do |spec_file|
-          file = File.new spec_file
-          builder.build_str file.read, spec_file
-        end
-
-        builder.build_str 'Opal::RSpec::Runner.autorun', '(exit)'
-        # builder.build_str 'exit', '(exit)'
-
+        builder.build_str main_code, 'opal_spec.rb'
         builder.to_s
+      end
+
+      def builder
+        @builder ||= begin
+          path_finder = Opal::HikePathFinder.new(paths)
+
+          Opal::Builder.new(
+            compiler_options: Opal::Processor.compiler_options,
+            stubs:            Opal::Processor.stubbed_files,
+            path_reader:      Opal::PathReader.new(path_finder),
+          )
+        end
+      end
+
+      def paths
+        Opal.paths + sprockets.paths + [spec_location]
+      end
+
+      def main_code(files = spec_files)
+        main_code = []
+        main_code << 'require "opal"'
+        main_code << 'require "opal-rspec"'
+        files.each do |file|
+          file = clean_spec_path(file)
+          main_code << %Q{require #{file.inspect}}
+        end
+        main_code << 'Opal::RSpec::Runner.autorun'
+        main_code.join("\n")
       end
 
       def spec_files
@@ -43,15 +53,15 @@ module Opal
       end
 
       def all_spec_files
-        spec_files_for_glob '**/*_spec{.js,}'
+        spec_files_for_glob '**/*_spec'
       end
 
       def spec_files_for_glob glob = '**'
-        Dir[root.join("#{spec_location}/#{glob}.{rb,opal}")]
+        Dir[root.join("#{spec_location}/#{glob}{,.js}.{rb,opal}")]
       end
 
       def clean_spec_path(path)
-        path.split("#{spec_location}/").flatten.last.gsub(/(\.rb|\.opal)/, '')
+        path.split("#{spec_location}/").flatten.last.gsub(/(\.js)?\.(rb|opal)$/, '')
       end
     end
   end
