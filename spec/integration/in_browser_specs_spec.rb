2f1c4615decab7852b4b510817808d34ad343afb
Elia Schito <elia@schito.me>
2015-03-20 00:59:20 +0100

    No more extensions in the browser spec page

--- spec/integration/in_browser_specs_spec.rb
+++ spec/integration/in_browser_specs_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 feature 'In-browser specs runner' do
   scenario 'runs all specs', :js do
     visit '/opal_spec'
-    page.should have_content('example_spec.js ')
-    page.should have_content('requires_opal_spec.js ')
-    page.should have_content('subdirectory/other_spec.js')
+    page.should have_content('example_spec ')
+    page.should have_content('requires_opal_spec ')
+    page.should have_content('subdirectory/other_spec ')
     page.should have_content('3 examples, 0 failures')
   end
 
   scenario "runs single spec file", :js do
     visit '/opal_spec'
-    click_link 'subdirectory/other_spec.js'
+    click_link 'subdirectory/other_spec'
 
-    page.should_not have_content('example_spec.js ')
-    page.should_not have_content('requires_opal_spec.js ')
-    page.should have_content('subdirectory/other_spec.js')
+    page.should_not have_content('example_spec ')
+    page.should_not have_content('requires_opal_spec ')
+    page.should have_content('subdirectory/other_spec ')
     page.should have_content('1 examples, 0 failures')
   end
 end
