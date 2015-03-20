0251a8ea9c0273383720022c9227a25031021b1b
Elia Schito <elia@schito.me>
2015-03-20 01:16:06 +0100

    Overload javascript_include_tag with loading code

--- app/helpers/opal_helper.rb
+++ app/helpers/opal_helper.rb
@@ -5,4 +5,16 @@ module OpalHelper
     javascript_tag js_code
   end
 
+  def javascript_include_tag(*sources)
+    sources_copy = sources.dup.tap(&:extract_options!)
+    sprockets = Rails.application.assets
+
+    script_tags = super
+
+    sources_copy.map do |source|
+      loading_code = Opal::Processor.load_asset_code(sprockets, source)
+      script_tags << javascript_tag(loading_code)
+    end
+    script_tags
+  end
 end
