0961a0270926058c7f65c69b7b8b44dd8e815de4
Elia Schito <elia@schito.me>
2015-03-20 01:06:12 +0100

    Remove obsolete helper

--- app/helpers/opal_helper.rb
+++ app/helpers/opal_helper.rb
@@ -5,14 +5,4 @@ module OpalHelper
     javascript_tag js_code
   end
 
-  def spec_include_tag(*sources)
-    options = sources.extract_options!.stringify_keys
-    path_options = options.extract!('protocol', 'extname').symbolize_keys
-    sources.uniq.map { |source|
-      tag_options = {
-        "src" => "/opal_spec_files/#{source}"
-      }.merge!(options)
-      content_tag(:script, "", tag_options)
-    }.join("\n").html_safe
-  end
 end
