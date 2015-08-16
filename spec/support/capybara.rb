92eb04e7b2cc08f7eb01d0e1b94b68a54eda5aac
Brady Wied <brady@bswtechconsulting.com>
2015-08-16 17:29:03 -0600

    Travis failed when local succeeded Given weirdness in travis w/ webkit + qt 4.x (see https://github.com/thoughtbot/capybara-webkit/issues/550#issuecomment-78237170), trying selenium/firefox

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,5 @@
 require 'capybara/rspec'
 
-require 'capybara-webkit'
-Capybara.javascript_driver = :webkit
+Capybara.javascript_driver = :selenium
+# Specs should run much faster than this but in case Travis takes longer, provide some cushion
+Capybara.default_wait_time = 10
