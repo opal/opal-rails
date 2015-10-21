40b18062c86dcef6d78cce62dcbabd9b2201da76
Brady Wied <brady@bswtechconsulting.com>
2015-10-21 13:26:57 -0600

    Run this from inside the app directory

--- spec/integration/rake_task_spec.rb
+++ spec/integration/rake_task_spec.rb
@@ -1,4 +1,10 @@
 describe 'Rake task' do
+  around do |ex|
+    Dir.chdir 'test_app' do
+      ex.run
+    end
+  end
+
   subject { `rake opal:spec` }
 
   it { is_expected.to match '3 examples, 0 failures' }
