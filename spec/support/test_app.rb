9c61d9abd5796f1d62a71b13b1c3e0259def9508
Elia Schito <elia@schito.me>
2015-12-24 01:20:59 +0100

    Test app cleanup

--- spec/support/test_app.rb
+++ spec/support/test_app.rb
@@ -1,9 +1,6 @@
 require 'rails'
 rails_version = Rails::VERSION::MAJOR
 ENV["RAILS_ENV"] = "test"
-# ENV['DATABASE_URL'] = 'sqlite3://localhost/:memory:'
-# ENV['DATABASE_URL'] = 'sqlite3://localhost/:memory:/test'
-# ENV['DATABASE_URL'] = 'sqlite3://:memory:'
 ENV['DATABASE_URL'] = 'sqlite3::memory:'
 require_relative "../../test_apps/rails#{rails_version}"
 
--- test_apps/rails4.rb
+++ test_apps/rails4.rb
@@ -41,17 +41,8 @@ end
 LAYOUT = <<-HTML
 <!DOCTYPE html>
 <html>
-<head>
-  <title>TestApp</title>
-  <%= stylesheet_link_tag    "application", :media => "all" %>
-  <%= javascript_include_tag "application" %>
-  <%= csrf_meta_tags %>
-</head>
-<body>
-
-<%= yield %>
-
-</body>
+<head><%= javascript_include_tag "application" %></head>
+<body><%= yield %></body>
 </html>
 HTML
 
