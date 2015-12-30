70eb166fc1f2f5b65a1b39eb5f17656604f6945b
Elia Schito <elia@schito.me>
2015-12-31 00:33:16 +0100

    Remove an old, now unnecessary, patch

--- lib/opal/rails.rb
+++ lib/opal/rails.rb
@@ -4,7 +4,6 @@ require 'opal-activesupport'
 
 require 'opal/rails/engine'
 require 'opal/rails/template_handler'
-require 'opal/rails/sprockets_cache_key_fix'
 require 'opal/rails/version'
 
 require 'jquery-rails'
