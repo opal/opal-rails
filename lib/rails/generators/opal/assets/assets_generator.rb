3e2386acd69ce64216f65bf966ddf8fe54dfb568
Michael <mibamur@gmail.com>
2016-07-03 10:02:04 +0900

    Update assets_generator.rb
    could not understand for what this?
    ```rb
    def initialize(*args)
      module_name = ::Rails::Generators.const_defined?('ModelHelpers') ? 'ModelHelpers' : 'ResourceHelpers'
      ::Rails::Generators.const_get(module_name).skip_warn = true
      super
    end
    ```
    
    everything worked better without it

--- lib/rails/generators/opal/assets/assets_generator.rb
+++ lib/rails/generators/opal/assets/assets_generator.rb
@@ -1,21 +1,11 @@
 require 'rails/generators/named_base'
-require 'rails/generators/resource_helpers'
 
 module Opal
   module Generators
     class AssetsGenerator < ::Rails::Generators::NamedBase
-      include ::Rails::Generators::ResourceHelpers
       source_root __dir__+'/templates'
-
-      def initialize(*args)
-    
-        module_name = ::Rails::Generators.const_defined?('ModelHelpers') ? 'ModelHelpers' : 'ResourceHelpers'
-        ::Rails::Generators.const_get(module_name).skip_warn = true
-        super
-      end
-
       def copy_opal
-        template 'javascript.js.rb', File.join('app/assets/javascripts', "#{controller_name.underscore}_view.js.rb")
+        template 'javascript.js.rb', File.join('app/assets/javascripts', class_path, "#{file_name}.js.rb")
       end
     end
   end
