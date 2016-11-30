0a87f6b4a53911a4e31489adbc37abe6b60fbbf0
Elia Schito <elia@schito.me>
2016-11-30 01:08:48 +0100

    Ensure the template has the correct content type
    fixes #72

---
+++ spec/integration/template_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'template handler' do
+  it 'has the correct content type' do
+    get '/application/with_assignments.js'
+    expect(response).to be_success
+    expect(response.headers['Content-Type']).to eq('text/javascript; charset=utf-8')
+  end
+end
