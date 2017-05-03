f7b1c8e7f7e99c455915415600c0580a28af9034
Elia Schito <elia@schito.me>
2017-05-04 01:04:41 +0200

    Remove Rails 5 deprecation

--- test_apps/rails5.rb
+++ test_apps/rails5.rb
@@ -12,7 +12,7 @@ module RailsApp
     config.root                                       = __dir__
     config.cache_classes                              = true
     config.eager_load                                 = false
-    config.serve_static_files                         = true
+    config.public_file_server.enabled                 = true
     config.public_file_server.headers                 = { 'Cache-Control' => 'public, max-age=3600' }
     config.consider_all_requests_local                = true
     config.action_controller.perform_caching          = false
