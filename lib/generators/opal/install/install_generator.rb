b4aa995cdeb98b3bcbf693485752f7c451c0ca1e
Elia Schito <elia@schito.me>
2021-03-03 20:00:57 +0100

    Add a missing newline when updating the application layout

--- lib/generators/opal/install/install_generator.rb
+++ lib/generators/opal/install/install_generator.rb
@@ -6,7 +6,8 @@ class Opal::InstallGenerator < Rails::Generators::Base
     template "application.js.rb", "app/assets/javascript/application.js.rb"
 
     # Add the javascript tag to the application head tag
-    gsub_file 'app/views/layouts/application.html.erb', %r{^( *)</head>},
-      '\1  <%= javascript_include_tag "application", "data-turbolinks-track": "reload" %>\1</head>'
+    gsub_file 'app/views/layouts/application.html.erb', %r{(\n *)</head>},
+      '\1  <%= javascript_include_tag "application", "data-turbolinks-track": "reload" %>' \
+      '\1</head>'
   end
 end
