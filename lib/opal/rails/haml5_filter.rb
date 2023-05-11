8b609890712c1d00e80b73fe1e0bb0ac9347d44b
hmdne <54514036+hmdne@users.noreply.github.com>
2023-05-11 09:52:43 +0200

    Support ESM mime type for HAML filter

--- lib/opal/rails/haml5_filter.rb
+++ lib/opal/rails/haml5_filter.rb
@@ -1,13 +1,17 @@
 module Haml::Filters::Opal
   include Haml::Filters::Base
 
+  def mime_type
+    ::Opal::Config.esm ? 'module' : 'text/javascript'
+  end
+
   def render_with_options ruby, options
     text = ::Opal.compile(ruby)
 
     if options[:format] == :html5
       type = ''
     else
-      type = " type=#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}"
+      type = " type=#{options[:attr_wrapper]}#{mime_type}#{options[:attr_wrapper]}"
     end
 
     text.rstrip!
--- lib/opal/rails/haml6_filter.rb
+++ lib/opal/rails/haml6_filter.rb
@@ -5,14 +5,18 @@ require "haml/filters/base"
 module Haml
   class Filters
     class Opal < Base
+      def mime_type
+      end
+      
       def compile(node)
         template = [:multi]
-        template << [:static, "<script type='text/javascript'>\n"]
+        template << [:static, "<script type='#{mime_type}'>\n"]
         template << [:static, ::Opal.compile(node.value[:text]) ]
         template << [:static, "\n</script>"]
         template
       end
     end
+    ::Opal::Config.esm ? 'module' : 'text/javascript'
   end
 end
 
