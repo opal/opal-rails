aad3806207fe750b79b6f60cad3225fc0e6229a3
Diego Scataglini <dscataglini@hedgeye.com>
2024-07-29 21:01:27 -0400

    renaming variable

--- app/helpers/opal_helper.rb
+++ app/helpers/opal_helper.rb
@@ -1,14 +1,14 @@
 require 'opal/sprockets'
 
 module OpalHelper
-  def opal_tag(opal_code = nil, html_options = {}, &block)
+  def opal_tag(opal_code_or_options = nil, html_options = {}, &block)
     if block_given?
-      html_options = opal_code if opal_code.is_a?(Hash)
-      opal_code = capture(&block)
+      html_options = opal_code_or_options if opal_code_or_options.is_a?(Hash)
+      opal_code_or_options = capture(&block)
     end
 
     compiler_options = Opal::Config.compiler_options.merge(requirable: false)
-    compiler = Opal::Compiler.new(opal_code, compiler_options)
+    compiler = Opal::Compiler.new(opal_code_or_options, compiler_options)
     js_code = compiler.compile
     javascript_tag html_options do
       js_code
